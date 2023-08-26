6bf74f5accec68ffd4c71740ee61bcd77978fd81
Babitk21 <77804801+Babitk21@users.noreply.github.com>
2023-08-27 01:12:59 +0530

    First Commit

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Button NextScene;
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    //void Update() {
+    //    NextScene.onClick.AddListener(LoadingNextScene);
+    //}
+    public void LoadingFirstScene() {
+        SceneManager.LoadScene(1);
+    }    
+    public void LoadingSecondScene() {
+        SceneManager.LoadScene(2);
+    }
+    public void LoadingStartingScene() {
+        SceneManager.LoadScene(0);
     }
 }
